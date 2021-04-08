a288ac4567e2256082c4c9ba416c8e8846f657d4
Katherine Tam <katherinemtam@gmail.com>
2021-04-08 16:10:40 -0700

    TDD getUser, setUser, updateUserInfo functions

---
+++ utils/local-storage-utils.js
@@ -0,0 +1,22 @@
+const USER = 'USER';
+
+export function getUser() {
+    const stringyUser = localStorage.getItem(USER);
+    const parsedUser = JSON.parse(stringyUser);
+    return parsedUser;
+}
+
+export function setUser(user) {
+    const stringyUser = JSON.stringify(user);
+    localStorage.setItem(USER, stringyUser);
+}
+
+export function updateUserInfo(questId, choice) {
+    const user = getUser();
+
+    user.hp += choice.hp;
+    user.gold += choice.gold;
+    user.completed[questId] = true;
+
+    setUser(user);
+}
