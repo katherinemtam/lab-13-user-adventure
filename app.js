cf6693463742fc4dcf51d6fdbbefa006be7d4700
Katherine Tam <katherinemtam@gmail.com>
2021-04-08 17:24:40 -0700

    add structure and functionality to home page(character creation)

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('index');
+import { setUser } from './utils/local-storage-utils.js';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const data = new FormData(form);
+
+    const userName = data.get('username');
+    const userClass = data.get('class');
+
+    const user = {
+        name: userName,
+        class: userClass,
+        hp: 35,
+        gold: 0,
+        completed: {}
+    };
+
+    setUser(user);
+
+    window.location = './map/index.html';
+});
